e7c38f4f2e6406d8fbb8b121186783088e3d3ef2
del226 <del226@lehigh.edu>
2023-03-23 15:20:22 -0400

    finished

--- Start/Ch_3/challenge_start.py
+++ Start/Ch_3/challenge_start.py
@@ -3,9 +3,9 @@
 
 import json
 import csv
+import datetime
 
 
-# open the data file and load the JSON
 with open("../../30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
 
@@ -13,3 +13,26 @@ with open("../../30DayQuakes.json", "r") as datafile:
 # 40 most significant seismic events, ordered by most recent
 # Header row: Magnitude, Place, Felt Reports, Date, and Google Map link
 # Date should be in the format of YYYY-MM-DD
+def getsig(x):
+    sig = x["properties"]["sig"]
+    return 0 if sig is None else sig
+
+sigquakes = sorted(data["features"], key=getsig,reverse=True)
+sigquakes= sigquakes[:40]
+sigquakes.sort(key=lambda e: e["properties"]["time"], reverse=True)
+header = ["Magnitude", "Place", "Felt Reports", "Significance", "Link", "Date"]
+rows = []
+
+for quake in sigquakes:
+    thedate = datetime.date.fromtimestamp(
+            int(quake["properties"]["time"]/1000))
+    rows.append([quake["properties"]["mag"],
+                 quake["properties"]["place"],
+                 0 if quake["properties"]["felt"] is None else quake["properties"]["felt"],
+                 quake["properties"]["sig"],
+                 quake["properties"]["url"],
+                 thedate])
+with open("sigquakes.csv", "w") as csvfile:
+    writer = csv.writer(csvfile, delimiter=",")
+    writer.writerow(header)
+    writer.writerows(rows)
